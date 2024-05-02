8eddb048a3ee0fa596d19eef785421f9618d2dbc
David Chibuzor <dchibuzor31@gmail.com>
2024-05-02 23:13:59 +0100

    week 8

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger')
+const nav = document.querySelector('.nav-2')
+
+hamburger.addEventListener('click', ()=> {
+    nav.classList.toggle('visible')
+}
+)
